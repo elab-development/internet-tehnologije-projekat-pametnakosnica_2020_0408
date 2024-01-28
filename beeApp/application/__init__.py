b408a4695e49b1448265e85f3a92f322941914e1
peglenko <kotlajastefan8@gmail.com>
2024-01-28 20:22:17 +0100

    added login and register functionalities with basic frontend representation

--- beeApp/application/__init__.py
+++ beeApp/application/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from application.config import ApplicationConfig
 from application.database import db
-from application.extensions import migrate, bcrypt, server_session
+from application.extensions import migrate, bcrypt, server_session, cors
 
 def create_app():
     app = Flask(__name__)
@@ -10,6 +10,7 @@ def create_app():
     bcrypt.init_app(app)
     
     #zato sto imamo server-side session onda ce default flaskov session biti na serveru
+    cors.init_app(app, supports_credentials=True)
     server_session.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
--- beeApp/application/extensions.py
+++ beeApp/application/extensions.py
@@ -1,7 +1,9 @@
 from flask_migrate import Migrate
 from flask_bcrypt import Bcrypt
 from flask_session import Session
+from flask_cors import CORS
 
 migrate = Migrate()
 bcrypt = Bcrypt()
 server_session = Session()
+cors = CORS()
